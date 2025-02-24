c6eb84d4960474888b49571c06ff161f2379e1e2
Avinashkumar945 <avinash732004kumar@gmail.com>
2025-02-24 23:48:40 +0530

    changes

---
+++ Pattern_DoubleHollowRightTriangles.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    if(n>1){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            if(j==i||j==1||i==n){
+            printf("*");
+            }
+            else{
+                printf(" ");
+            }
+        }
+        for(int k=2;k<=2*(n-i+1)-1;k++){
+            printf(" ");
+        }
+        for(int j=1;j<=i;j++){
+            if(i==1||i==n||j==i||j==1){
+            printf("*");
+            }
+            else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+        }
+       else{
+           printf("Shape Not Possible");
+       }
+    return 0;
+}
