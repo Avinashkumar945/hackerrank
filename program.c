7687f85027cdb257dcad9e7a10eea004d225bb7e
Avinashkumar945 <avinash732004kumar@gmail.com>
2025-05-01 23:59:41 +0530

    Create program.c

---
+++ program.c
@@ -0,0 +1,113 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+
+/*
+ * Complete the 'strcmp' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. STRING s1
+ *  2. STRING s2
+ */
+
+int mstrcmp(char* s1, char* s2) {
+   int i=0;
+   size_t s1lent=strlen(s1);
+   size_t s2lent=strlen(s2);
+    while((s1[i]!='\0'&&i<s1lent)&&(s2[i]!='\0'&&i<s2lent)){
+      if(s1[i]!=s2[i]){
+         break;
+      }
+      i++;
+    }
+  int diff =s1lent-s2lent;
+  if(diff!=0){
+    if(diff<0){
+      return -diff;
+    }
+    else{
+      return diff;
+    }
+  }
+   return s1[i]-s2[i];
+ 
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char* s1 = readline();
+
+    char* s2 = readline();
+
+    int result = mstrcmp(s1, s2);
+
+    fprintf(fptr, "%d\n", result);
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
